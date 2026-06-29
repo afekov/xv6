1782bc17bc0ebeda2e986bff27b58dbbe0df170d
afekov <afek2007@gmail.com>
2026-06-29 15:55:16 +0300

    lab finished
    It has recently been discovered that every possible hashing algorithm produces the same value for the phrase "Bruce Schneier" -- Bruce Schneier.

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -12,7 +12,7 @@ sys_exit(void)
   int n;
   argint(0, &n);
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -43,7 +43,7 @@ sys_sbrk(void)
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -54,12 +54,13 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
   argint(0, &n);
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n)
+  {
+    if (killed(myproc()))
+    {
       release(&tickslock);
       return -1;
     }
@@ -69,12 +70,41 @@ sys_sleep(void)
   return 0;
 }
 
-
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+int MAXPAGES = 256;
+
+int sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
+  uint64 ad;
+  argaddr(0, &ad);
+  char *buf = (char *)ad;
+  int npages;
+  argint(1, &npages);
+  uint64 abitsad;
+  argaddr(2, &abitsad);
+  pte_t *curpage;
+  char myabits[MAXPAGES / 8];
+  for (int i = 0; i < npages; i++)
+  {
+    curpage = walk(myproc()->pagetable, (uint64)buf + i * PGSIZE, 0);
+    if (curpage == 0)
+    {
+      return -1;
+    }
+    if ((PTE_FLAGS(*curpage) & (1 << 6)) > 0)
+    {
+      myabits[i / 8] = myabits[i / 8] | (1 << (i % 8));
+      *curpage = (pte_t)((uint64)(*curpage) - (1 << 6));
+    }
+    else
+    {
+      myabits[i / 8] = myabits[i / 8] & (0xff - (1 << (i % 8)));
+    }
+  }
+  if (copyout(myproc()->pagetable, abitsad, myabits, (npages + 7) / 8) < 0)
+  {
+    return -1;
+  }
   return 0;
 }
 #endif
